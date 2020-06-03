7b2afa000d3cc8b90afef5511ceb7c37ab507a10
Jean Dames <jeancdames@gmail.com>
2020-06-03 16:01:20 -0500

    working on implementing fullcalendar

--- src/main/java/org/launchcode/maintainer/controllers/HomeController.java
+++ src/main/java/org/launchcode/maintainer/controllers/HomeController.java
@@ -1,9 +1,9 @@
 package org.launchcode.maintainer.controllers;
 
 import org.launchcode.maintainer.models.Appointment;
-import org.launchcode.maintainer.models.DisplayCalendar;
 import org.launchcode.maintainer.models.data.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -14,25 +14,20 @@ import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.Year;
 import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
     @Autowired
     private AppointmentRepository appointmentRepository;
-    private DisplayCalendar displayCalendar;
 
     @RequestMapping("")
     public String index(Model model) {
-
-        LocalDateTime today = LocalDateTime.now();
-        Month currentMonth = today.getMonth();
-        Integer currentYear = today.getYear();
-
-        model.addAttribute("title", "Dashboard");
-//        model.addAttribute("cal", displayCalendar.getThisMonthData());
-        model.addAttribute("today", today);
+        Calendar cal = Calendar.getInstance();
+        model.addAttribute("appointments", appointmentRepository.findAll(Sort.by("dateTime").ascending()));
         return "index";
     }
 
--- src/main/java/org/launchcode/maintainer/models/data/AppointmentRepository.java
+++ src/main/java/org/launchcode/maintainer/models/data/AppointmentRepository.java
@@ -1,12 +1,17 @@
 package org.launchcode.maintainer.models.data;
 
 import org.launchcode.maintainer.models.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.awt.print.Pageable;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 //@Transactional
-public interface AppointmentRepository extends CrudRepository<Appointment, Integer> {
+public interface AppointmentRepository extends JpaRepository<Appointment, Integer> {
+
 }
