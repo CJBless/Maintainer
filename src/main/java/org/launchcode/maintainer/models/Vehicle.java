68103cb4cae187bc72a1993ea8dd5e4282a3862c
Jean Dames <jeancdames@gmail.com>
2020-06-30 08:47:55 -0500

    removed commented out field

--- src/main/java/org/launchcode/maintainer/models/Vehicle.java
+++ src/main/java/org/launchcode/maintainer/models/Vehicle.java
@@ -31,7 +31,6 @@ public class Vehicle extends AbstractEntity {
     @JoinColumn(name = "vehicle_id")
     private Set<Appointment> appointments = new HashSet<>();
 
-//    private String image=
 
     public Vehicle() {}
 
